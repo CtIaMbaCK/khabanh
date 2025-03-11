983a7c5e8f4baa64ece183abd26e0229678a5c7d
Dat <hathiendat09072004@gmail.com>
2025-03-11 18:09:32 +0700

    update

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 // Danh sách các âm thanh đã chuẩn bị sẵn
 const sounds = [
 //   new Audio("/audio/mixkit-male-voice-cheer-victory-2011.wav"),
-  new Audio("/audio/BIỆT GIANG HỒ REMIX.mp3"),
-  // new Audio('sound3.mp3'),
+  new Audio("./BIỆT GIANG HỒ REMIX.mp3"),
+//   new Audio('sound3.mp3'),
   // new Audio('sound4.mp3')
 ];
 
